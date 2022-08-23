71bb09a8cfd8f91e5e2949a489fffc5d758c7599
FrankFMY <pryanishnikovartem@gmail.com>
2022-08-23 04:18:23 +0300

    Some useless comments deleted

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -49,51 +49,12 @@ const Home: React.FC = () => {
     window.scrollTo(0, 0);
   };
 
-  // Если изменили параметры и был первый рендер
   React.useEffect(() => {
-    // if (isMounted.current) {
-    //   const params = {
-    //     categoryId: categoryId > 0 ? categoryId : null,
-    //     sortProperty: sort.sortProperty,
-    //     currentPage,
-    //   };
-
-    //   const queryString = qs.stringify(params, { skipNulls: true });
-
-    //   navigate(`/?${queryString}`);
-    // }
-
-    // const params = qs.parse(window.location.search.substring(1)) as unknown as SearchPizzaParams;
-    // const sortObj = sortList.find((obj) => obj.sortProperty === params.sortBy);
-    // dispatch(
-    //   setFilters({
-    //     searchValue: params.search,
-    //     categoryId: Number(params.category),
-    //     currentPage: Number(params.currentPage),
-    //     sort: sortObj || sortList[0],
-    //   }),
-    // );
-
+   
     getPizzas();
-    // isMounted.current = true;
+    
   }, [categoryId, sort.sortProperty, searchValue, currentPage]);
 
-  // Парсим параметры при первом рендере
-  // React.useEffect(() => {
-  //   if (window.location.search) {
-  //     const params = qs.parse(window.location.search.substring(1)) as unknown as SearchPizzaParams;
-  //     const sort = sortList.find((obj) => obj.sortProperty === params.sortBy);
-  //     dispatch(
-  //       setFilters({
-  //         searchValue: params.search,
-  //         categoryId: Number(params.category),
-  //         currentPage: Number(params.currentPage),
-  //         sort: sort || sortList[0],
-  //       }),
-  //     );
-  //   }
-  //   isMounted.current = true;
-  // }, []);
 
   const pizzas = items.map((obj: any) => <PizzaBlock key={obj.id} {...obj} />);
   const skeletons = [...new Array(6)].map((_, index) => <Skeleton key={index} />);
